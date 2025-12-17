f8ccfdfcbc555d58f21a710fd28adefda0ee861d
nabil <arobasenabil@gmail.com>
2025-12-17 12:51:48 +0900

    fixed next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-
+const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
